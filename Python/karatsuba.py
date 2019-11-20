f9237ecb75607a512e127bb516bca9f81de48547
yashshah2820 <201851150@iiitvadodara.ac.in>
2019-11-20 11:18:06 +0530

    files moved, idr

--- Python/karatsuba.py
+++ Python/karatsuba.py
@@ -1,30 +1,21 @@
-def karatsuba(x, y):
-    if len(str(x)) == 1 or len(str(y)) == 1:
+def karat(x,y):
+    if len(str(x)) < 2 or len(str(y)) < 2:
         return x*y
-    else:
-        m = max(len(str(x)), len(str(y)))
-        m2 = m // 2
 
-        a = x // 10**(m2)
-        b = x % 10**(m2)
-        c = y // 10**(m2)
-        d = y % 10**(m2)
+    n = max(len(str(x)),len(str(y))) // 2
 
-        z0 = karatsuba(b, d)
-        z1 = karatsuba((a+b), (c+d))
-        z2 = karatsuba(a, c)
+    a = x // 10**(n)
+    b = x % 10**(n)
+    c = y // 10**(n)
+    d = y % 10**(n)
 
-        return (z2 * 10**(2*m2)) + ((z1 - z2 - z0) * 10**(m2)) + (z0)
+    z0 = karat(b,d)
+    z1 = karat((a+b), (c+d))
+    z2 = karat(a,c)
 
+    return ((10**(2*n))*z2)+((10**n)*(z1-z2-z0))+z0
 
-a = 12
-b = 5
-print(karatsuba(a, b))
+a = int(input("\nEnter the first number : "))
+b = int(input("\n\nEnter the second number : "))
 
-a = 7125486542
-b = 23456583
-print(karatsuba(a, b))
-
-a = 742136284561247845962145632140
-b = 214510102547219842103256421872
-print(karatsuba(a, b))
+print("Multiplication : " + str(karat(a,b)))
